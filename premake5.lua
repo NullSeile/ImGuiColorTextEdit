340ce86d78817c0cb9ba8dcb92c533c32ab2b45d
nullptr <eliesbertran@gmail.com>
2022-01-27 22:29:17 +0100

    Added premake5.lua and .gitignore

---
+++ premake5.lua
@@ -0,0 +1,34 @@
+project "ImGuiColorTextEdit"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"TextEditor.h",
+		"TextEditor.cpp"
+	}
+
+    includedirs {
+		"../../../OpenGL-Core/%{IncludeDir.ImGui}"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
